5537eac81da7b32690bc228dd37fa482ad5f18fa
Matthew Baker <fimbaz@gmail.com>
2017-05-03 15:28:00 -0700

    Added neighbor query struct

--- shash/src/grid.rs
+++ shash/src/grid.rs
@@ -15,3 +15,30 @@ impl <P> Grid<P> {
                                                               
 }
                   
+struct NeighborQuery<'t,P: 't + HasPos>{
+    map: &'t FnvHashMap<BucketPos,P>,
+    query: &'t Region,
+    bucket: &'t Vec<P>,
+}
+impl<'t,P: HasPos> Iterator for NeighborQuery<'t,P>{
+    type Item = &'t P;
+    fn next(&mut self) -> Option<&'t P>{
+        None
+    }
+}
+/*
+fn get_neighbors(map:&FnvHashMap<BucketPos,Vec<Pos>>,bpos: BucketPos) -> Vec<&Pos>{
+    let x = bpos.0.x;
+    let y = bpos.0.y;
+    let width = if x == 0 { 1 } else { 2 };
+    let height = if y == 0 { 1 } else { 2 };
+    let nhood = Region {x,y,width,height};
+    let npos = Pos { x, y };
+    for pos in vec![BucketPos::new(x,y),BucketPos::new(x+width,y),BucketPos::new(x,y+height),BucketPos::new(x+width,y+height)]{
+        if let Some(neighbors) = map.get(&pos){
+            return neighbors.iter().filter(|n|nhood.contains(*n)).collect()
+        }
+    }
+    
+}
+*/
--- shash/src/main.rs
+++ shash/src/main.rs
@@ -4,20 +4,6 @@ use rect::{BucketPos,Pos,Region,HasPos};
 use fnv::FnvHashMap;
 use std::hash::Hash;
 
-fn get_neighbors(map:&FnvHashMap<BucketPos,Vec<Pos>>,bpos: BucketPos) -> Vec<&Pos>{
-    let x = bpos.0.x;
-    let y = bpos.0.y;
-    let width = if x == 0 { 1 } else { 2 };
-    let height = if y == 0 { 1 } else { 2 };
-    let nhood = Region {x,y,width,height};
-    let npos = Pos { x, y };
-    for pos in vec![BucketPos::new(x,y),BucketPos::new(x+width,y),BucketPos::new(x,y+height),BucketPos::new(x+width,y+height)]{
-        if let Some(neighbors) = map.get(&pos){
-            return neighbors.iter().filter(|n|nhood.contains(*n)).collect()
-        }
-    }
-    
-}
 
 fn main(){
 
@@ -29,6 +15,6 @@ fn main(){
             value.push(Pos::new(i,j));
         }
     }
-    let pos2 = BucketPos::new(1,1);
+    let pos2 = BucketPos::new(2,2);
     println!("{:?}",map.get(&pos2));
 }
