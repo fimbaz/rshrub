114c91ab30b322f41603db2c43f1c16200ee2ff3
Matthew Baker <fimbaz@gmail.com>
2017-05-08 02:09:13 -0700

    widget polymorphism technically working now

--- shash/src/ui/core.rs
+++ shash/src/ui/core.rs
@@ -1,5 +1,6 @@
 use rustty::{Terminal,Event};
 use rustty::ui::core::Widget;
+use std::borrow::BorrowMut;
 pub struct UI{
     terminal: Terminal,
     widgets: Vec<Box<Widget>>
@@ -13,12 +14,20 @@ impl UI{
             while let Some(Event::Key(ch)) = self.terminal.get_event(10).unwrap() {
                 match ch {
                     'q' => {return;},
+                    'd' => {self.draw_boxes()},
                     _ => continue
+                }
+                for  widget in &mut self.widgets{
+                    widget.draw(&mut self.terminal);
                 }
             }
         }
     }
-
+    pub fn draw_boxes(&mut self){
+        for  widget in &mut self.widgets{
+            let none: () = widget.draw_box();
+        }
+    }
     pub fn add_widget(&mut self,widget: Box<Widget>){
         self.widgets.push(widget);
     }
